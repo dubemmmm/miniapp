3d034f5391198f57fb46e47b8f14e30273a388b8
Chidubem Onwuchuluba <101866658+dubemmmm@users.noreply.github.com>
2025-08-23 11:59:09 -0400

    Update urls.py

--- miniapp/urls.py
+++ miniapp/urls.py
@@ -22,6 +22,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.landing, name='landing')
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
