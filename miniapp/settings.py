ffa2d71204b8cdff4e250b25c0893869109294ca
Chidubem Onwuchuluba <chidubemonwuchuluba@Chidubems-MacBook-Air.local>
2025-08-25 17:21:33 -0400

    fix: Status files renderng

--- miniapp/settings.py
+++ miniapp/settings.py
@@ -134,10 +134,6 @@ WHITENOISE_KEEP_ONLY_HASHED_FILES = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  
-]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
