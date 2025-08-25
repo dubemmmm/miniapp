303c2cdd6d79389bd733b4320baf2403dac56db7
Chidubem Onwuchuluba <chidubemonwuchuluba@Chidubems-MacBook-Air.local>
2025-08-25 17:35:00 -0400

    commit

--- miniapp/settings.py
+++ miniapp/settings.py
@@ -24,7 +24,7 @@ TEMP_DIR = os.path.join(BASE_DIR, "templates")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default=None)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cwproperty.org', 'www.cwproperty.org', 'mini-app-fw2l4.ondigitalocean.app', '127.0.0.1']
 
