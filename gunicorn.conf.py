845a33f421269b4baa734e186ab9cc73ae3e8824
Chidubem Onwuchuluba <chidubemonwuchuluba@Chidubems-MacBook-Air.local>
2025-09-07 18:37:52 -0400

    updated gunicorn.conf.py to bind for health check

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 # gunicorn.conf.py
-bind = '0.0.0.0:8000'  # Bind to all interfaces, port 8000
+bind = '0.0.0.0:8080'  # Bind to all interfaces, port 8000
 workers = 1           # Use 1 worker for low memory (512 MB Droplet)
 timeout = 120         # Increase timeout to 120 seconds for sync tasks
 loglevel = 'info'     # Log level for debugging
