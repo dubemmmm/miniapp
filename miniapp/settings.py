d0e093497d70cae924da41ace415801239f12b7b
Chidubem Onwuchuluba <chidubemonwuchuluba@Chidubems-MacBook-Air.local>
2025-10-09 22:51:24 -0400

    added ip address to allowed_host

--- miniapp/settings.py
+++ miniapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default=None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cwproperty.org', 'www.cwproperty.org', 'mini-app-fw2l4.ondigitalocean.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['cwproperty.org', 'www.cwproperty.org', 'mini-app-fw2l4.ondigitalocean.app', '127.0.0.1', 'localhost', '18.188.180.17']
 
 
 # Application definition
