6d06cd2a2e1eeb7ee5202444c09dbf1c0c424ee3
Chidubem Onwuchuluba <101866658+dubemmmm@users.noreply.github.com>
2025-10-27 12:15:42 -0400

    changed it to offplan.cwlagos.com

--- miniapp/settings.py
+++ miniapp/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['cwproperty.org', 'www.cwproperty.org', 
                  'mini-app-fw2l4.ondigitalocean.app', '127.0.0.1', 
-                 'localhost', '18.188.180.17', 'offshore.cwlagos.com',]
+                 'localhost', '18.188.180.17', 'offplan.cwlagos.com',]
 
 
 # Application definition
